b327768b617fff8fdab02237edf0dcc77ec259ba
Nikolai Diachkov <ypypy28@yandex.ru>
2022-05-13 01:39:13 +0300

    add cli args parser

--- parse_uiks/__main__.py
+++ parse_uiks/__main__.py
@@ -1,4 +1,13 @@
-﻿import main
+﻿import argparse
+import main
 
 
-main.run()
+parser = argparse.ArgumentParser(description="Parse uiks from state izbirkoms' webpage")
+parser.add_argument("url", metavar="URL", type=str, nargs='?',
+                    help="link to the state izbirkoms' webpage with uiks")
+parser.add_argument("-o", default="out", type=str, nargs='?',
+                    help="path to the directory where to put result")
+
+args = parser.parse_args()
+
+main.run(start_url=args.url, out_dir=args.o)
