cb4479ab62dbc5a7c21c7095e0755133f9605b00
Nikolai Diachkov <ypypy28@yandex.ru>
2022-05-21 12:41:14 +0300

    end program immediately if there is no chromedriver, get help description from init

--- parsix/__main__.py
+++ parsix/__main__.py
@@ -1,6 +1,7 @@
 ﻿import argparse
 import signal
 import sys
+from parsix import __doc__ as description
 from parsix import main
 from parsix.config import REGIONS, OUTPUT_DIR_NAME, SHOW_CHROME
 from shutil import which
@@ -16,11 +17,12 @@ def start():
     signal.signal(signal.SIGINT, exit_gracefully)
     if not which('chromedriver'):
         print("You need to install chromedriver, "
-            "download it from here https://chromedriver.chromium.org/home")
+              "download it from here https://chromedriver.chromium.org/home")
+        sys.exit(1)
 
     parser = argparse.ArgumentParser(
         prog="parsix",
-        description="Election commissions parser",
+        description=description,
     )
     parser.add_argument("--output-dir", "-o", default=OUTPUT_DIR_NAME, nargs='?',
                         help="path to the directory where to put results")
@@ -33,8 +35,8 @@ def start():
     args = parser.parse_args()
 
     main.run(region=args.region,
-            out_dir=args.output_dir,
-            show_chrome=args.show_chrome)
+             out_dir=args.output_dir,
+             show_chrome=args.show_chrome)
 
 
 if __name__ == "__main__":
